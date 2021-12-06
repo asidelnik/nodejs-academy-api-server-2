783fc849963172358c6c3742db053f653a11746e
Amos Sidelnik <asidelnik@gmail.com>
2021-12-06 08:59:21 +0200

    user-controller - logoutUser & deleteUser - completed & debugged

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require('express-validator')
 const InvalidParamError = require('./../errors/InvalidParamError')
+const InternalError = require('../errors/InvalidParamError')
 const UsersService = require('./../services/users-service')
 const LoginFailure = require('../errors/LoginFailure')
 
@@ -45,24 +46,30 @@ async function loginUser(req, res, next) {
 async function logoutUser(req, res, next) {
     try {
         //TODO 1. take user from req and 
-        const { user, token } = req.body
-        // Clean user token (via UsersService)
-        await UsersService.deleteToken(user, token)
+        const { user } = req.body
+        // Clean user (on MongoDB) token (via UsersService)
+        const id = user.id
+        const userWithoutToken = await UsersService.removeUserToken(id)
         //TODO 2. save user (via UsersService)
         //TODO 3. return user without token
-        res.status(201).send({ user })
+        res.status(201).send({ userWithoutToken })
     } catch (e) {
         //TODO 4. throw proper error
-        next(e)
+        throw InternalError("Failed to logout: " + e)
     }
 }
 
 async function deleteUser(req, res, next) {
+    const userId = req.params.id
     try {
         //TODO 1. delete user (via UserService)
+        // const user = await User.findOne({ _id: userId })
+        const deletedUser = await UsersService.deleteUser(userId)
         //TODO 2. send deleted user
+        return res.status(200).json(deletedUser)
     } catch (e) {
         //TODO 3. throw proper error
+        return res.status(404).json({ error: `No user with id ${userId}` })
     }
 }
 
